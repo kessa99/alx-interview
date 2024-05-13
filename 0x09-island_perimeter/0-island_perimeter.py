cfbad86e5a3081a5579eb3f97c1f5f7e1f94a3f3
kessa99 <kipre325@gmail.com>
2024-05-13 02:11:47 +0000

    update island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def island_perimeter(grid):
+    perimetre = 0
+    # parcourir chaque ligne de la grille
+    for ligne in range(len(grid)):
+        # parcourir chaque colonne de la grille
+        for colonne in range(len(grid[ligne])):
+            # si la cellule est un morceau de terre
+            if grid[ligne][colonne] == 1:
+                # vérifier si la cellule est à l'intérieur de la grille
+                voisins = 0
+                if ligne > 0 and grid[ligne - 1][colonne] == 0:
+                    voisins += 1
+                if ligne < len(grid) - 1 and grid[ligne + 1][colonne] == 0:
+                    voisins += 1
+                if colonne > 0 and grid[ligne][colonne - 1] == 0:
+                    voisins += 1
+                if colonne < len(grid[ligne]) - 1:
+                    if grid[ligne][colonne + 1] == 0:
+                        voisins += 1
+                perimetre += voisins
+    return perimetre
